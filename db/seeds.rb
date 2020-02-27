c21072e6d52bea6416ed28893ad02080dcd682be
Natali <butenkonata0@gmail.com>
2020-02-27 14:44:51 -0500

    Add simple models for author, group and book

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+# add simple Book with author and group
+
+
+author = Author.create!(name: "Walter Scott")
+group = Group.create!(name: "Adventure")
+book = Book.new(title: "Ivanhoe", image: "https://prodimage.images-bn.com/pimages/9781593082468_p0_v1_s550x406.jpg")
+book.authors << author
+book.groups << group
+book.save
