6645fa6540c7f0c35a25d46f4fbfeeb681ab7221
natali2001 <49942113+natali2001@users.noreply.github.com>
2020-03-03 03:16:55 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,14 @@ Rails.application.routes.draw do
   get 'books/update'
   get 'books/edit'
   get 'books/destroy'
+
+  get 'authors/index'
+  get 'authors/create'
+  get 'authors/new'
+  get 'authors/show'
+  get 'authors/update'
+  get 'authors/edit'
+  get 'authors/destroy'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
@@ -17,6 +25,6 @@ Rails.application.routes.draw do
   get 'books' => 'books#index'
 
   resources :books
-
+  resources :authors
 
 end
